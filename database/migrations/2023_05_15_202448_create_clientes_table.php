e8335b2e3f76268ae328a56efbb553c44df9a2f2
unknown <SENA@CALAPRCEAFSD5P7.FormacionSena.RED>
2023-05-15 16:37:38 -0500

    Se agrega email a tabla clientes

--- database/migrations/2023_05_15_202448_create_clientes_table.php
+++ database/migrations/2023_05_15_202448_create_clientes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('clientes', function (Blueprint $table) {
             $table->id(); 
             $table->string ('nombre', 100);   
+            $table->string ('email', 100);   
             $table->integer('telefono');
             $table->unsignedBigInteger('usuario_id');
  
